68e3352ca09dd634aeeca1fc15d96811568170ca
MorMundHS-MA <MorMundHS-MA@users.noreply.github.com>
2020-12-30 22:43:28 +0100

    Added new check-in/checkout flow with new layout

--- CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/model/RoomVisit.java
+++ CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/model/RoomVisit.java
@@ -12,7 +12,8 @@ import java.util.Date;
 @NoArgsConstructor
 public class RoomVisit {
     @ManyToOne
-    @JoinColumn(updatable = false, insertable = false)
+    @JoinColumn
+    @NonNull
     private Room room;
 
     @Id
@@ -27,7 +28,8 @@ public class RoomVisit {
     private Date end = null;
 
     @ManyToOne
-    @JoinColumn(updatable = false, insertable = false)
+    @JoinColumn
+    @NonNull
     private Besucher visitor;
 
     public RoomVisit(Besucher visitor, Room room, Date start) {
@@ -39,16 +41,31 @@ public class RoomVisit {
     @lombok.Data
     @NoArgsConstructor
     public static class Data {
+        @NonNull
         private String roomId;
+        @NonNull
+        private String roomName;
+        private int roomCapacity;
+        private int currentVisitorCount;
         private String visitorEmail;
         private Date start = null;
         private Date end = null;
 
-        public Data(RoomVisit visit) {
+        public Data(RoomVisit visit, int currentVisitorCount) {
             this.roomId = visit.room.getId();
+            this.roomName = visit.room.getName();
+            this.roomCapacity = visit.room.getMaxCapacity();
+
             this.visitorEmail = visit.visitor.getEmail();
+            this.currentVisitorCount = currentVisitorCount;
             this.start = visit.start;
             this.end = visit.end;
+        }
+
+        public Data(Room.Data roomData) {
+            this.roomId = roomData.getRoomId();
+            this.roomName = roomData.getRoomName();
+            this.roomCapacity = roomData.getMaxCapacity();
         }
     }
 }
---
+++ CTT-Github/src/main/resources/static/checkInForm.js
@@ -0,0 +1,80 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const storage = window.localStorage;
+    const emailText = document.getElementById("email-text");
+    const postfixDropDown = document.getElementById("email-postfix");
+    let emailPostfix = null;
+
+    // Show content that requires JavaScript to function
+    showJSEnabled();
+
+    // Setup the initial drop-down selection and handle the drop down event
+    postFixChanged(postfixDropDown.value);
+    postfixDropDown.addEventListener("change", (event) => {
+        postFixChanged(event.target.value);
+    })
+
+    // Inject the actual email into the final form submitted to the server
+    document.getElementById("submit-form").addEventListener("submit", () => {
+        const autoSignIn = document.getElementById("email-auto-sign-in");
+        const combinedEmail = getFullEmail();
+
+        if(autoSignIn.checked) {
+            storage.setItem('email', combinedEmail);
+        } else {
+            storage.removeItem('email');
+        }
+
+        document.getElementById("submit-form-email").value = combinedEmail;
+    })
+
+    document.getElementById("submit-form-checkout").addEventListener("submit", () => {
+        const autoSignIn = document.getElementById("email-auto-sign-in");
+        const combinedEmail = getFullEmail();
+
+        if(autoSignIn.checked) {
+            storage.setItem('email', combinedEmail);
+        } else {
+            storage.removeItem('email');
+        }
+
+        document.getElementById("submit-form-email-checkout").value = combinedEmail;
+    })
+
+    // Change the form layout to allow the user to checkout of a previous room
+    document.getElementById("checkout-button").addEventListener("click", () => doSignOut());
+
+    function postFixChanged(postfix) {
+        switch (postfix) {
+            case "student":
+                emailText.placeholder = "Matrikelnummer";
+                emailText.type = "number";
+                emailPostfix = "@stud.hs-mannheim.de";
+                break;
+            case "internal":
+                emailText.placeholder = "Nutzername";
+                emailText.type = "text";
+                emailPostfix = "@hs-mannheim.de";
+                break;
+            default:
+            case "external":
+                emailText.placeholder = "Vollständige E-Mail";
+                emailText.type = "email";
+                emailPostfix = null;
+                break;
+        }
+    }
+
+    function getFullEmail() {
+        return emailText.value + emailPostfix;
+    }
+
+    function showJSEnabled() {
+        document.querySelectorAll(".no-js").forEach((elem) => elem.classList.add("hidden"))
+        document.querySelectorAll(".js-enabled").forEach((elem) => elem.classList.remove("js-enabled"))
+    }
+
+    function doSignOut() {
+        document.querySelectorAll(".checkin-only").forEach((elem) => elem.classList.add("hidden"))
+        document.querySelectorAll(".checkout-only").forEach((elem) => elem.classList.remove("checkout-only"))
+    }
+});
