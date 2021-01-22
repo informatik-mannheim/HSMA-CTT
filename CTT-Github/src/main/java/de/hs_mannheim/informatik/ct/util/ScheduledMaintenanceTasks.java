9e98dd9bc6d320130481e620092326a590e01649
oliverhummel <o.hummel@hs-mannheim.de>
2021-01-23 00:51:55 +0100

    Backup für H2DB eingefügt

--- CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/util/ScheduledMaintenanceTasks.java
+++ CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/util/ScheduledMaintenanceTasks.java
@@ -1,12 +1,18 @@
 package de.hs_mannheim.informatik.ct.util;
 
+import java.io.File;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import de.hs_mannheim.informatik.ct.persistence.repositories.RoomRepository;
 import de.hs_mannheim.informatik.ct.persistence.repositories.VeranstaltungsBesuchRepository;
 import de.hs_mannheim.informatik.ct.persistence.services.RoomVisitService;
 
@@ -19,14 +25,24 @@ public class ScheduledMaintenanceTasks {
     @Autowired
     private RoomVisitService roomVisitService;
     
+    @Autowired
+    private RoomRepository roomRepository;
+    
 	@Autowired
 	private VeranstaltungsBesuchRepository repoVB;
+	
+	@Value("${spring.datasource.driverClassName}")
+	private String db;
     
  //   @Scheduled(fixedRate = 30 * 60 * 1000) // Every 30 minutes
 	@Scheduled(cron = "0 55 3 * * *")	// 3:55 AM
     public void doMaintenance() {
         signOutAllVisitors(LocalTime.parse("21:00:00"));
+        
         deleteExpiredVisitRecords(Period.ofWeeks(4));
+        loescheAlteBesuche();
+        
+        doDbBackup();
     }
 
     public void signOutAllVisitors(LocalTime forcedEndTime) {
@@ -39,8 +55,27 @@ public class ScheduledMaintenanceTasks {
         roomVisitService.deleteExpiredRecords(recordLifeTime);
     }
     
-	@Scheduled(cron = "0 55 2 * * *")	// 2:55 AM
 	public void loescheAlteBesuche() {
 		repoVB.loescheAlteBesuche();
+	}
+	
+	public void doDbBackup() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		String target = "bak/" + LocalDate.now().format(formatter)+ "-db-backup" + ".zip";
+
+		if ("org.h2.Driver".contentEquals(db)) {
+			roomRepository.backupH2DB(target);
+
+			File[] files = new File("bak/").listFiles();
+
+			for (File file : files) {
+				long diff = new Date().getTime() - file.lastModified();
+
+				if (diff > 7 * 24 * 60 * 60 * 1000) {		// delete after 7 days
+					file.delete();
+				}
+			}
+		} // H2DB
+
 	}
 }
