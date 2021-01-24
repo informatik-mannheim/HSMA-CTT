39101972a97b22a51a035f779813b99c54440d65
MorMundHS-MA <23387065+MorMundHS-MA@users.noreply.github.com>
2021-01-24 21:33:26 +0100

    Integrated room contact tracing with existing tracing (#22)
    * Integrated room contact tracing with existing tracing
    
    * Added queries for visitor record expiration and end of day sign-out (#20)
    
    * Added queries for visitor record expiration and end of day sign-out
    
    * Added test cases for maintenance
    
    * Cleanups
    
    * Backup für H2DB eingefügt
    
    * Kommentar angepasst
    
    Co-authored-by: MorMundHS-MA <MorMundHS-MA@users.noreply.github.com>
    Co-authored-by: oliverhummel <o.hummel@hs-mannheim.de>
    
    * Integrated room contact tracing with existing tracing
    
    Co-authored-by: MorMundHS-MA <MorMundHS-MA@users.noreply.github.com>
    Co-authored-by: oliverhummel <o.hummel@hs-mannheim.de>

--- CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/persistence/repositories/RoomVisitRepository.java
+++ CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/persistence/repositories/RoomVisitRepository.java
@@ -1,39 +1,47 @@
 package de.hs_mannheim.informatik.ct.persistence.repositories;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import de.hs_mannheim.informatik.ct.model.Besucher;
 import de.hs_mannheim.informatik.ct.model.Room;
 import de.hs_mannheim.informatik.ct.model.RoomVisit;
-import de.hs_mannheim.informatik.ct.model.VeranstaltungsBesuch;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Date;
-import java.util.List;
-
+import de.hs_mannheim.informatik.ct.model.RoomVisitContact;
 
 public interface RoomVisitRepository extends JpaRepository<RoomVisit, Long> {
-    @Query("SELECT visit " +
-            "FROM RoomVisit visit " +
-            "WHERE visit.visitor = :visitor and visit.end is null")
-    List<RoomVisit> findNotCheckedOutVisits(@Param(value = "visitor") Besucher visitor);
+	@Query("SELECT visit " +
+			"FROM RoomVisit visit " +
+			"WHERE visit.visitor = :visitor and visit.end is null")
+	List<RoomVisit> findNotCheckedOutVisits(@Param(value = "visitor") Besucher visitor);
 
-    @Query("SELECT COUNT (visit) " +
-            "FROM RoomVisit visit " +
-            "WHERE visit.room = :room and visit.end is null ")
-    int getRoomVisitorCount(@Param(value = "room") Room room);
+	@Query("SELECT COUNT (visit) " +
+			"FROM RoomVisit visit " +
+			"WHERE visit.room = :room and visit.end is null ")
+	int getRoomVisitorCount(@Param(value = "room") Room room);
 
-    /**
-     * Finds all visitors that haven't checked out yet.
-     * @return All visitors that haven't checked out yet.
-     */
-    @Query("SELECT visit " +
-            "FROM RoomVisit visit " +
-            "WHERE visit.end is null")
-    List<RoomVisit> findNotCheckedOutVisits();
+	/**
+	 * Finds all visitors that haven't checked out yet.
+	 * @return All visitors that haven't checked out yet.
+	 */
+	@Query("SELECT visit " +
+			"FROM RoomVisit visit " +
+			"WHERE visit.end is null")
+	List<RoomVisit> findNotCheckedOutVisits();
 
-    void deleteByEndBefore(Date end);
+	void deleteByEndBefore(Date end);
+
+	@Query("SELECT NEW de.hs_mannheim.informatik.ct.model.RoomVisitContact(visitTarget, visitOther) " +
+			"FROM RoomVisit visitTarget," +
+			"RoomVisit visitOther " +
+			"WHERE visitTarget.visitor = :visitor AND " +
+			"visitTarget.room = visitOther.room AND " +
+			"visitTarget.start <= visitOther.end AND " +
+			"visitOther.start <= visitTarget.end " +
+			"ORDER BY visitTarget.start")
+	List<RoomVisitContact> findVisitsWithContact(@Param(value = "visitor") Besucher visitor);
+	
 }
--- CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/persistence/services/RoomVisitService.java
+++ CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/persistence/services/RoomVisitService.java
@@ -1,94 +1,106 @@
 package de.hs_mannheim.informatik.ct.persistence.services;
 
-import de.hs_mannheim.informatik.ct.model.Besucher;
-import de.hs_mannheim.informatik.ct.model.Room;
-import de.hs_mannheim.informatik.ct.model.RoomVisit;
-import de.hs_mannheim.informatik.ct.persistence.repositories.RoomVisitRepository;
-import lombok.NonNull;
-import lombok.val;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import static de.hs_mannheim.informatik.ct.util.TimeUtil.convertToDate;
+import static de.hs_mannheim.informatik.ct.util.TimeUtil.convertToLocalDate;
+import static de.hs_mannheim.informatik.ct.util.TimeUtil.convertToLocalTime;
 
-import java.time.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import static de.hs_mannheim.informatik.ct.util.TimeUtil.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import de.hs_mannheim.informatik.ct.model.Besucher;
+import de.hs_mannheim.informatik.ct.model.Room;
+import de.hs_mannheim.informatik.ct.model.RoomVisit;
+import de.hs_mannheim.informatik.ct.model.RoomVisitContact;
+import de.hs_mannheim.informatik.ct.persistence.repositories.RoomVisitRepository;
+import lombok.NonNull;
+import lombok.val;
 
 @Service
 public class RoomVisitService {
 
-    @Autowired
-    private RoomVisitRepository roomVisitRepository;
+	@Autowired
+	private RoomVisitRepository roomVisitRepository;
 
-    public RoomVisit visitRoom(Besucher visitor, Room room) {
-        return roomVisitRepository.save(new RoomVisit(visitor, room, new Date()));
-    }
+	public RoomVisit visitRoom(Besucher visitor, Room room) {
+		return roomVisitRepository.save(new RoomVisit(visitor, room, new Date()));
+	}
 
-    /**
-     * Checks the visitor out from all currently checked-in visits and returns the visits.
-     * Their should usually be only one checked in visit at a time.
-     *
-     * @param visitor The visitor who is checked out of their visits.
-     * @return The rooms the visitor was checked into.
-     */
-    @NonNull
-    public List<RoomVisit> checkOutVisitor(@NonNull Besucher visitor) {
-        List<RoomVisit> notSignedOutVisits = getCheckedInRoomVisits(visitor);
-        notSignedOutVisits.forEach((visit) -> {
-            visit.setEnd(new Date());
-            roomVisitRepository.save(visit);
-        });
+	/**
+	 * Checks the visitor out from all currently checked-in visits and returns the visits.
+	 * Their should usually be only one checked in visit at a time.
+	 *
+	 * @param visitor The visitor who is checked out of their visits.
+	 * @return The rooms the visitor was checked into.
+	 */
+	@NonNull
+	public List<RoomVisit> checkOutVisitor(@NonNull Besucher visitor) {
+		List<RoomVisit> notSignedOutVisits = getCheckedInRoomVisits(visitor);
+		notSignedOutVisits.forEach((visit) -> {
+			visit.setEnd(new Date());
+			roomVisitRepository.save(visit);
+		});
 
-        return notSignedOutVisits;
-    }
+		return notSignedOutVisits;
+	}
 
-    @NonNull
-    public List<RoomVisit> getCheckedInRoomVisits(@NonNull Besucher visitor) {
-        return roomVisitRepository.findNotCheckedOutVisits(visitor);
-    }
+	@NonNull
+	public List<RoomVisit> getCheckedInRoomVisits(@NonNull Besucher visitor) {
+		return roomVisitRepository.findNotCheckedOutVisits(visitor);
+	}
 
-    public void checkOutAllVisitors(@NonNull LocalTime forcedVisitEndTime) {
-        // Setting the end time in Java makes it easier to set it correctly.
-        // Concretely, visits will always end on the same day even if processing was delayed.
-        val notCheckedOut = roomVisitRepository.findNotCheckedOutVisits();
-        val updatedVisits = notCheckedOut
-                .stream()
-                .map((roomVisit -> {
-                    val startTime = convertToLocalTime(roomVisit.getStart());
-                    val startDate = convertToLocalDate(roomVisit.getStart());
+	public void checkOutAllVisitors(@NonNull LocalTime forcedVisitEndTime) {
+		// Setting the end time in Java makes it easier to set it correctly.
+		// Concretely, visits will always end on the same day even if processing was delayed.
+		val notCheckedOut = roomVisitRepository.findNotCheckedOutVisits();
+		val updatedVisits = notCheckedOut
+				.stream()
+				.map((roomVisit -> {
+					val startTime = convertToLocalTime(roomVisit.getStart());
+					val startDate = convertToLocalDate(roomVisit.getStart());
 
-                    if (startTime.isBefore(forcedVisitEndTime)) {
-                        val endDate = startDate.atTime(forcedVisitEndTime);
-                        roomVisit.setEnd(convertToDate(endDate));
-                    } else if (startDate.isBefore(LocalDate.now())) {
-                        // Visit started yesterday after forced sign-out time, sign-out at midnight yesterday instead
-                        val endDate = startDate.atTime(LocalTime.parse("23:59:59"));
-                        roomVisit.setEnd(convertToDate(endDate));
-                    } else {
-                        return null;
-                    }
+					if (startTime.isBefore(forcedVisitEndTime)) {
+						val endDate = startDate.atTime(forcedVisitEndTime);
+						roomVisit.setEnd(convertToDate(endDate));
+					} else if (startDate.isBefore(LocalDate.now())) {
+						// Visit started yesterday after forced sign-out time, sign-out at midnight yesterday instead
+						val endDate = startDate.atTime(LocalTime.parse("23:59:59"));
+						roomVisit.setEnd(convertToDate(endDate));
+					} else {
+						return null;
+					}
 
-                    return roomVisit;
-                }))
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
+					return roomVisit;
+				}))
+				.filter(Objects::nonNull)
+				.collect(Collectors.toList());
 
-        roomVisitRepository.saveAll(updatedVisits);
-    }
+		roomVisitRepository.saveAll(updatedVisits);
+	}
 
-    public int getVisitorCount(@NonNull Room room) {
-        return roomVisitRepository.getRoomVisitorCount(room);
-    }
+	public int getVisitorCount(@NonNull Room room) {
+		return roomVisitRepository.getRoomVisitorCount(room);
+	}
 
-    public boolean isRoomFull(@NonNull Room room) {
-        return getVisitorCount(room) >= room.getMaxCapacity();
-    }
+	public boolean isRoomFull(@NonNull Room room) {
+		return getVisitorCount(room) >= room.getMaxCapacity();
+	}
 
-    public void deleteExpiredRecords(Period recordLifeTime) {
-        val oldestAllowedRecord = LocalDateTime.now().minus(recordLifeTime);
-        roomVisitRepository.deleteByEndBefore(convertToDate(oldestAllowedRecord));
-    }
+	public void deleteExpiredRecords(Period recordLifeTime) {
+		val oldestAllowedRecord = LocalDateTime.now().minus(recordLifeTime);
+		roomVisitRepository.deleteByEndBefore(convertToDate(oldestAllowedRecord));
+	}
+
+	public List<RoomVisitContact> getVisitorContacts(@NonNull Besucher visitor) {
+		return roomVisitRepository.findVisitsWithContact(visitor);
+	}
+
 }
