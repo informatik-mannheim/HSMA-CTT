fb9c5863b32136b937c5495896cef09b4c08ca22
MorMundHS-MA <MorMundHS-MA@users.noreply.github.com>
2021-02-10 17:39:42 +0100

    Added index for visitor email column

--- CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/model/Visitor.java
+++ CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/model/Visitor.java
@@ -1,22 +1,23 @@
 package de.hs_mannheim.informatik.ct.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
 
 import javax.persistence.*;
-
-import lombok.*;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(indexes = @Index(columnList = "email"))
 public class Visitor {
 	@Id
 	@GeneratedValue
 	private Long id;
 
-	@Column(unique = true)
+	@Column(unique = true, nullable = false)
 	@NonNull
 	private String email;
 
