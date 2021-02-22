aa1785a54039c910d15b9b2b725082623864ac80
Erik <erik.reiter@stud.hs-mannheim.de>
2021-02-22 13:23:39 +0100

    Added encryption with AttributeEncryptor to Visitor email
    added testcases to VisitorRepositoryTest for reading encrypted and decrypted values

--- CTT-Github/src/test/java/de/hs_mannheim/informatik/ct/persistence/repositories/VisitorRepositoryTest.java
+++ CTT-Github/src/test/java/de/hs_mannheim/informatik/ct/persistence/repositories/VisitorRepositoryTest.java
@@ -7,10 +7,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Collection;
-import java.util.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -21,9 +25,16 @@ public class VisitorRepositoryTest {
     @Autowired
     private VisitorRepository visitorRepository;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     private final Visitor visitor1 = new Visitor("12345@stud.hs-mannheim.de");
     private final Visitor visitor2 = new Visitor("13337@stud.hs-mannheim.de");
     private final Visitor visitor3 = new Visitor("77777@stud.hs-mannheim.de");
+
+    //visitors for encryption testing
+    private final String decryptedEmail = "987654321@stud.hs-mannheim.de";
+    private final Visitor visitor4 = new Visitor(decryptedEmail);
 
     private void befuelleDatenbank() {
         Room room = new Room("A008", 2);
@@ -40,6 +51,7 @@ public class VisitorRepositoryTest {
         entityManager.persist(visitor1);
         entityManager.persist(visitor2);
         entityManager.persist(visitor3);
+        entityManager.persist(visitor4);
 
         besuch1.setEnde(new Date());
         besuch2.setEnde(new Date());
@@ -62,4 +74,33 @@ public class VisitorRepositoryTest {
                         besuch.getBesucherEmail().equals(visitor2.getEmail())
         ));
     }
+
+    @Test
+    public void readDecryptedViaRepo() {
+        befuelleDatenbank();
+
+        Optional<Visitor> visitorOptional = visitorRepository.findByEmail(decryptedEmail);
+        Assertions.assertEquals(decryptedEmail, visitorOptional.get().getEmail());
+    }
+
+    @Test
+    public void readEncryptedViaJDBC() {
+        String query = "SELECT * FROM  visitor";
+
+        jdbcTemplate.query(query, (ResultSetExtractor<Map>) rs -> {
+            HashMap<String,String> mapRet= new HashMap<String,String>();
+            while(rs.next()){
+                mapRet.put(rs.getString("ID"),rs.getString("EMAIL"));
+            }
+            Assertions.assertEquals(false,mapRet.containsValue("987654321@stud.hs-mannheim.de"));
+
+            return mapRet;
+        });
+
+
+
+
+    }
+
+
 }
