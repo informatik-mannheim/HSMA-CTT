c3308cae339817e1fe188f7ffe6109608491893f
Erik <erik.reiter@stud.hs-mannheim.de>
2021-02-25 12:00:55 +0100

    removed getBuildingName function
    renamed building attribute

--- CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/model/Room.java
+++ CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/model/Room.java
@@ -4,7 +4,6 @@ import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.util.regex.Pattern;
 
 @Data
 @Entity
@@ -17,7 +16,7 @@ public class Room {
     @NonNull
     private String name;
 
-    private String building;
+    private String buildingName;
 
     private int maxCapacity;
 
@@ -25,17 +24,6 @@ public class Room {
 
     public String getId() {
         return getName();
-    }
-
-    public String getBuildingName() {
-        // TODO: Store actual building names in db
-        // Matches rooms like A008 or A007a, which have the building as the initial letter
-        val roomWithBuildingMatcher = Pattern.compile("[A-Z]\\d+\\w?");
-        if(roomWithBuildingMatcher.matcher(name).matches()) {
-            return name.substring(0, 1);
-        } else {
-            return "?";
-        }
     }
 
     @lombok.Data
@@ -54,7 +42,7 @@ public class Room {
             roomName = room.getName();
             roomId = room.getId();
             maxCapacity = room.getMaxCapacity();
-            building = room.getBuilding();
+            building = room.getBuildingName();
         }
     }
 }
