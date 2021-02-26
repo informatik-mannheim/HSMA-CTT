de7ae4bdc4d30fc4cfdb840d9efce495363c0ac2
MorMundHS-MA <MorMundHS-MA@users.noreply.github.com>
2021-02-26 19:54:48 +0100

    Added overview page for downloading room printouts per building

--- CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/controller/PrintOutController.java
+++ CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/controller/PrintOutController.java
@@ -1,24 +1,32 @@
 package de.hs_mannheim.informatik.ct.controller;
 
+import de.hs_mannheim.informatik.ct.persistence.services.BuildingService;
 import de.hs_mannheim.informatik.ct.persistence.services.DynamicContentService;
 import de.hs_mannheim.informatik.ct.persistence.services.RoomService;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
-@RestController
+@Controller
 @RequestMapping("printout")
 public class PrintOutController {
     @Autowired
     private RoomService roomService;
+
+    @Autowired
+    private BuildingService buildingService;
 
     @Autowired
     private DynamicContentService contentService;
@@ -33,13 +41,27 @@ public class PrintOutController {
     private String host;
 
     @GetMapping(value = "/rooms")
-    public ResponseEntity<StreamingResponseBody> getRoomPrintout(HttpServletRequest request) {
-        val outFileName = "QRCodes.docx";
+    public String getRoomPrintoutList(Model model) {
+        model.addAttribute("buildings", buildingService.getAllBuildings());
+
+        return "rooms/roomPrintout";
+    }
+
+    @GetMapping(value = "/rooms/{building}")
+    public ResponseEntity<StreamingResponseBody> getRoomPrintout(
+            @PathVariable(value = "building") String building,
+            HttpServletRequest request) {
+        val roomsInBuilding = buildingService.getAllRoomsInBuilding(building);
+        if (roomsInBuilding.size() == 0) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Building not found or empty");
+        }
+
+        val outFileName = String.format("Gebäude %s.docx", building);
 
         StreamingResponseBody responseBody = outputStream -> {
             try {
                 contentService.writeRoomsPrintOutDocx(
-                        roomService.all(),
+                        roomsInBuilding,
                         outputStream,
                         room -> utilities.getUriToLocalPath(
                                 RoomController.getRoomCheckinPath(room),
