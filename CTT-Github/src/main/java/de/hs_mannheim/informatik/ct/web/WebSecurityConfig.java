ae21aea6eb7384a77a6eab1bfc64ca5ca61c4c05
oliverhummel <oliver@zehnkampf.net>
2021-02-27 19:05:03 +0100

    Passwörter durch Bcyrpt-Hashes ersetzt (#55)
    Co-authored-by: oliverhummel <o.hummel@hs-mannheim.de>

--- CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/web/WebSecurityConfig.java
+++ CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/web/WebSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
 @Configuration
@@ -17,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 		.antMatchers("/", "/besuch", "/templates/datenschutz.html").permitAll()
 		.antMatchers("/neu", "/neuVer", "/veranstaltungen", "/templates/veranstaltungsliste.html").access("hasAnyRole('USER', 'PROF', 'ADMIN')")
-		.antMatchers("/suche", "/suchen", "/liste", "/loeschen", "/download", "/h2-console/**").access("hasRole('ADMIN')")
+		.antMatchers("/suche", "/suchen", "/liste", "/loeschen", "/download", "/h2-console/**","/r/import").access("hasRole('ADMIN')")
 		.and().formLogin().loginPage("/login").permitAll()
 	    .and().csrf().ignoringAntMatchers("/h2-console/**")
 	    .and().headers().frameOptions().sameOrigin();	
@@ -25,14 +27,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void globalSecurityConfiguration(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER");
-		auth.inMemoryAuthentication().withUser("prof").password("{noop}prof").roles("PROF");
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("PROF","ADMIN");
+//		System.out.println("hash -> " + passwordEncoder().encode("passwordYouWannaEncode"));
+
+		auth.inMemoryAuthentication().withUser("user").password("$2a$10$WUJevKFYLHfIheVZ3yv7J.7uIHeoPV8fAb9wFqdW50kFD8O4EWJ4u").roles("USER");
+		auth.inMemoryAuthentication().withUser("prof").password("$2a$10$WUJevKFYLHfIheVZ3yv7J.7uIHeoPV8fAb9wFqdW50kFD8O4EWJ4u").roles("PROF");
+		auth.inMemoryAuthentication().withUser("admin").password("$2a$10$nW.GjVHey9UA47Xv8V8yHe5WQ67rNLsI3bjdi8gIM/28MufxGc53a").roles("PROF","ADMIN");
+				
+		auth.inMemoryAuthentication().withUser("k.albert@hs-mannheim.de").password("$2a$10$GTSP6jJBQD58zd3RQz62zeHIGfCEjMp7Lcjg77jkXh.zbvT9qBKCa").roles("PROF","ADMIN");
 	}
 	
 	@Bean
 	public SpringSecurityDialect springSecurityDialect() {
 	    return new SpringSecurityDialect();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
 	}
 
 }
