e8f0365fc69861881ad4818b7191db8948c136a2
L4trax <66115873+L4trax@users.noreply.github.com>
2021-03-05 14:28:29 +0100

    GNU OpenSourceLicense (#61)
    * GNU OpenSourceLicense
    
    Co-authored-by: MorMundHS-MA <MorMundHS-MA@users.noreply.github.com>

--- CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/util/ScheduledMaintenanceTasks.java
+++ CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/util/ScheduledMaintenanceTasks.java
@@ -1,5 +1,23 @@
 package de.hs_mannheim.informatik.ct.util;
 
+/*
+ * Corona Tracking Tool der Hochschule Mannheim
+ * Copyright (C) 2021 Hochschule Mannheim
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import java.io.File;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -17,6 +35,7 @@ import de.hs_mannheim.informatik.ct.persistence.repositories.VeranstaltungsBesuc
 import de.hs_mannheim.informatik.ct.persistence.services.RoomVisitService;
 
 
+
 /**
  * Schedules maintenance queries such as signing out visitors at the end of the day and deleting expired personal data
  */
@@ -24,24 +43,24 @@ import de.hs_mannheim.informatik.ct.persistence.services.RoomVisitService;
 public class ScheduledMaintenanceTasks {
     @Autowired
     private RoomVisitService roomVisitService;
-    
+
     @Autowired
     private RoomRepository roomRepository;
-    
+
 	@Autowired
 	private VeranstaltungsBesuchRepository repoVB;
-	
+
 	@Value("${spring.datasource.driverClassName}")
 	private String db;
-    
+
  //   @Scheduled(fixedRate = 30 * 60 * 1000) // Every 30 minutes
 	@Scheduled(cron = "0 55 3 * * *")	// 3:55 AM
     public void doMaintenance() {
         signOutAllVisitors(LocalTime.parse("21:00:00"));
-        
+
         deleteExpiredVisitRecords(Period.ofWeeks(4));
         loescheAlteBesuche();
-        
+
         doDbBackup();
     }
 
@@ -54,11 +73,11 @@ public class ScheduledMaintenanceTasks {
     public void deleteExpiredVisitRecords(Period recordLifeTime) {
         roomVisitService.deleteExpiredRecords(recordLifeTime);
     }
-    
+
 	public void loescheAlteBesuche() {
 		repoVB.loescheAlteBesuche();
 	}
-	
+
 	public void doDbBackup() {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		String target = "bak/" + LocalDate.now().format(formatter)+ "-db-backup" + ".zip";
