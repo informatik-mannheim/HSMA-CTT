fa188dabbf763cbffea42e08743e546e2a3551b7
MorMundHS-MA <MorMundHS-MA@users.noreply.github.com>
2021-03-22 21:28:12 +0100

    Refactored contact tracing

---
+++ CTT-Github/src/test/java/de/hs_mannheim/informatik/ct/persistence/services/ContactTracingServiceTest.java
@@ -0,0 +1,67 @@
+package de.hs_mannheim.informatik.ct.persistence.services;
+
+/*
+ * Corona Tracking Tool der Hochschule Mannheim
+ * Copyright (C) 2021 Hochschule Mannheim
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import de.hs_mannheim.informatik.ct.model.Visitor;
+import org.apache.xmlbeans.impl.tool.XSTCTester.TestCase;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(SpringExtension.class)
+public class ContactTracingServiceTest extends TestCase {
+    @TestConfiguration
+    static class ContactTracingServiceTestTestContextConfig {
+        @Bean
+        public ContactTracingService service() {
+            return new ContactTracingService();
+        }
+    }
+
+    @Autowired
+    private ContactTracingService contactTracingService;
+
+    @MockBean
+    private RoomVisitService roomVisitService;
+
+    @MockBean
+    private EventVisitService eventVisitService;
+
+    @Test
+    public void ContactTracing_CallsAllOtherServices() {
+        contactTracingService.getVisitorContacts(new Visitor());
+
+        verify(roomVisitService, times(1))
+                .getVisitorContacts(any());
+
+        verify(eventVisitService, times(1))
+                .getVisitorContacts(any());
+    }
+}
