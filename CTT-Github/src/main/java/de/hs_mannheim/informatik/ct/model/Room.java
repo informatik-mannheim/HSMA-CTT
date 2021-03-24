3b7f4a01ceeeacb9ea2e510c26414d42d3cf7d94
Linus <linus.bennin@gmail.com>
2021-03-25 00:00:02 +0100

    Room pin stays static for Room
    simple loop is looking if the room already exists and sets RoomPin to the old Room pin if they match

--- CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/model/Room.java
+++ CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/model/Room.java
@@ -43,6 +43,7 @@ public class Room {
         this.buildingName = buildingName;
         this.maxCapacity = maxCapacity;
         this.roomPin = String.format("%04d", new Random().nextInt(10000));
+       // System.out.println(roomPin);
     }
 
     public String getId() {
--- CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/persistence/DbInit.java
+++ CTT-Github/src/main/java/de/hs_mannheim/informatik/ct/persistence/DbInit.java
@@ -20,11 +20,15 @@ package de.hs_mannheim.informatik.ct.persistence;
 
 import de.hs_mannheim.informatik.ct.model.Room;
 import de.hs_mannheim.informatik.ct.persistence.repositories.RoomRepository;
+import de.hs_mannheim.informatik.ct.persistence.services.RoomService;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 
 
 @Service
@@ -35,12 +39,44 @@ public class DbInit implements CommandLineRunner {
     @Autowired
     private RoomRepository roomsRepo;
 
+
+    // For testing uncomment (we dont read Rooms from csv, for testing we initialize them here)
+
+
+/*    public Optional<Room> findByName(String roomName) {
+        try {
+            return roomsRepo.findByNameIgnoreCase(roomName);
+        } catch (IncorrectResultSizeDataAccessException e) {
+            return roomsRepo.findById(roomName);
+        }
+    }
+
+    public Room saveRoom(@NonNull Room room) {
+        System.out.println("Room Name this: " + room.getName());
+        System.out.println("Room Name find: " + findByName(room.getName()).get().getName());
+
+        if (findByName(room.getName()).get().getName().equals(room.getName())) {
+            System.out.println("True");
+
+            System.out.println("Room Pin before: " + room.getRoomPin());
+            room.setRoomPin(findByName(room.getName()).get().getRoomPin());
+            System.out.println("Room Pin after: " + room.getRoomPin());
+
+        }
+        return roomsRepo.save(room);
+    }*/
+
     @Override
     public void run(String... args) {
-        if(serverEnvironment.equals("dev")) {
+        if (serverEnvironment.equals("dev")) {
+
             roomsRepo.save(new Room("A007a", "A", 3));
             roomsRepo.save(new Room("test", "test", 12));
             roomsRepo.save(new Room("A210", "A", 19));
+
+            // saveRoom(new Room("A007a", "A", 3));
+            // saveRoom(new Room("test", "test", 12));
+            // saveRoom(new Room("A210", "A", 19));
         }
     }
 
