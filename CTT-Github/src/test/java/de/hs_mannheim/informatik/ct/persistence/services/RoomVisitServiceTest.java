07a81a931410e74f66e181c8dc99d7c8e220cc3b
MorMundHS-MA <MorMundHS-MA@users.noreply.github.com>
2021-04-13 11:32:44 +0200

    Fixed RoomVisitService breaking with checkout reason

--- CTT-Github/src/test/java/de/hs_mannheim/informatik/ct/persistence/services/RoomVisitServiceTest.java
+++ CTT-Github/src/test/java/de/hs_mannheim/informatik/ct/persistence/services/RoomVisitServiceTest.java
@@ -18,19 +18,24 @@ package de.hs_mannheim.informatik.ct.persistence.services;
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+
 import de.hs_mannheim.informatik.ct.model.Room;
-import de.hs_mannheim.informatik.ct.model.Visitor;
 import de.hs_mannheim.informatik.ct.model.RoomVisit;
+import de.hs_mannheim.informatik.ct.model.Visitor;
 import de.hs_mannheim.informatik.ct.persistence.RoomVisitHelper;
 import de.hs_mannheim.informatik.ct.persistence.repositories.RoomVisitRepository;
 import de.hs_mannheim.informatik.ct.persistence.repositories.VisitorRepository;
+import lombok.NonNull;
 import lombok.val;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -38,12 +43,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.*;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static de.hs_mannheim.informatik.ct.util.TimeUtil.convertToLocalDateTime;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.lessThan;
@@ -57,11 +62,6 @@ class RoomVisitServiceTest {
         public RoomVisitService service() {
             return new RoomVisitService();
         }
-
-        @Bean
-        public DateTimeService dateTimeService() {
-            return new DateTimeService();
-        }
     }
 
     @Autowired
@@ -72,6 +72,9 @@ class RoomVisitServiceTest {
 
     @MockBean
     private VisitorRepository visitorRepository;
+
+    @MockBean
+    private DateTimeService dateTimeService;
 
     @Captor
     private ArgumentCaptor<List<RoomVisit>> roomVisitCaptor;
@@ -89,42 +92,11 @@ class RoomVisitServiceTest {
     }
 
     @Test
-    void checkOutAllVisitors() {
-        val roomVisitHelper = new RoomVisitHelper(new Room("Test", "Test", 20));
-        val expected = new ArrayList<RoomVisit>();
-
-        val yesterday = LocalDate.now().minusDays(1);
-        val forcedEndTime = LocalTime.parse("18:00:00");
-        val visitor = new Visitor("1");
-        // No sign out yesterday
-        expected.add(roomVisitHelper.generateVisit(
-                visitor,
-                yesterday.atTime(LocalTime.parse("14:00:00")),
-                yesterday.atTime(forcedEndTime))); // Expected Value, removed in mock call
-        // No sign out today
-        expected.add(roomVisitHelper.generateVisit(
-                visitor,
-                LocalDate.now().atTime(LocalTime.parse("14:00:00")),
-                LocalDate.now().atTime(forcedEndTime)));
-        // Sign in yesterday after auto sign-in time
-        expected.add(roomVisitHelper.generateVisit(
-                visitor,
-                yesterday.atTime(forcedEndTime.plusHours(2)),
-                yesterday.atTime(LocalTime.parse("23:59:59"))));
-
-        val autoSignOutCandidates =
-                expected.stream()
-                        .peek((roomVisit -> roomVisit.setEndDate(null)))
-                        .collect(Collectors.toList());
-
-        // Sign in today after auto sign-in time
-        autoSignOutCandidates.add(roomVisitHelper.generateVisit(
-                visitor,
-                LocalDate.now().atTime(forcedEndTime.plusHours(2)),
-                null));
+    void autoCheckAutoVisitor_HadCheckedOutProperly() {
+        val forcedEndTime = LocalTime.of(18, 0);
 
         Mockito.when(roomVisitRepository.findNotCheckedOutVisits())
-                .thenReturn(autoSignOutCandidates);
+                .thenReturn(Collections.emptyList());
 
         roomVisitService.checkOutAllVisitors(forcedEndTime);
 
@@ -135,8 +107,77 @@ class RoomVisitServiceTest {
                 .saveAll(roomVisitCaptor.capture());
 
         val capturedArgument = roomVisitCaptor.getValue();
+        assertThat(capturedArgument.size(), equalTo(0));
+    }
 
-        assertThat(capturedArgument, is(expected));
+    @Test
+    void autoCheckAutoVisitor_NoCheckoutYesterday() {
+        val forcedEndTime = LocalTime.of(18, 0);
+        val today = LocalDate.of(2021, Month.APRIL, 2);
+        val yesterday = today.minusDays(1);
+
+        val checkInTime = LocalDateTime.of(yesterday, LocalTime.of(14, 0));
+        val now = LocalDateTime.of(today, forcedEndTime.plusHours(1));
+
+        val expectedCheckout = LocalDateTime.of(yesterday, forcedEndTime);
+
+        Mockito.when(dateTimeService.getNow())
+                .thenReturn(now);
+
+        testAutoCheckout(checkInTime, forcedEndTime, expectedCheckout);
+    }
+
+    @Test
+    void autoCheckAutoVisitor_NoCheckoutToday() {
+        val forcedEndTime = LocalTime.of(18, 0);
+        val today = LocalDate.of(2021, Month.APRIL, 2);
+
+        val checkInTime = LocalDateTime.of(today, LocalTime.of(14, 0));
+        val now = LocalDateTime.of(today, forcedEndTime.plusHours(1));
+
+        val expectedCheckout = LocalDateTime.of(today, forcedEndTime);
+
+        Mockito.when(dateTimeService.getNow())
+                .thenReturn(now);
+
+        testAutoCheckout(checkInTime, forcedEndTime, expectedCheckout);
+    }
+
+    /**
+     * Someone checked in yesterday AFTER the auto-checkout. They should be checked out at midnight.
+     */
+    @Test
+    void autoCheckAutoVisitor_NoCheckoutYesterdayAfterAutoCheckout() {
+        val forcedEndTime = LocalTime.of(18, 0);
+        val today = LocalDate.of(2021, Month.APRIL, 2);
+        val yesterday = today.minusDays(1);
+
+        val checkInTime = LocalDateTime.of(yesterday, LocalTime.of(18, 1));
+        val now = LocalDateTime.of(today, forcedEndTime.plusHours(1));
+
+        val expectedCheckout = LocalDateTime.of(yesterday, LocalTime.of(23, 59, 59));
+
+        Mockito.when(dateTimeService.getNow())
+                .thenReturn(now);
+
+        testAutoCheckout(checkInTime, forcedEndTime, expectedCheckout);
+    }
+
+    /**
+     * Someone checked in today AFTER the auto-checkout. They should not be checked out today.
+     */
+    @Test
+    void autoCheckAutoVisitor_NoCheckoutTodayAfterAutoCheckout() {
+        val forcedEndTime = LocalTime.of(18, 0);
+        val today = LocalDate.of(2021, Month.APRIL, 2);
+
+        val checkInTime = LocalDateTime.of(today, LocalTime.of(18, 1));
+        val now = LocalDateTime.of(today, forcedEndTime.plusHours(1));
+
+        Mockito.when(dateTimeService.getNow())
+                .thenReturn(now);
+
+        testAutoCheckout(checkInTime, forcedEndTime, null);
     }
 
     @Test
@@ -157,5 +198,37 @@ class RoomVisitServiceTest {
         val expectedDate = LocalDateTime.now().minusWeeks(4);
 
         assertThat(Duration.between(actualDate, expectedDate).toMinutes(), is(lessThan(1L)));
+    }
+
+    private void testAutoCheckout(@NonNull LocalDateTime checkInTime, @NonNull LocalTime forcedEndTime, LocalDateTime expectedCheckoutTime) {
+        val visitor = new Visitor("1");
+        val visit = new RoomVisitHelper(new Room("A", "A007a", 2)).generateVisit(
+                visitor,
+                checkInTime,
+                null
+        );
+
+        Mockito.when(roomVisitRepository.findNotCheckedOutVisits())
+                .thenReturn(Collections.singletonList(visit));
+
+        roomVisitService.checkOutAllVisitors(forcedEndTime);
+
+        Mockito
+                .verify(
+                        roomVisitRepository,
+                        Mockito.times(1))
+                .saveAll(roomVisitCaptor.capture());
+
+        val capturedArgument = roomVisitCaptor.getValue();
+        if (expectedCheckoutTime == null) {
+            // No one to be checked out
+            assertThat(capturedArgument.size(), equalTo(0));
+        } else {
+            assertThat(capturedArgument.size(), equalTo(1));
+            val checkedOutVisit = capturedArgument.get(0);
+            assertThat(convertToLocalDateTime(checkedOutVisit.getEndDate()), equalTo(expectedCheckoutTime));
+            assertThat(checkedOutVisit.getCheckOutSource(), equalTo(RoomVisit.CheckOutSource.AutomaticCheckout));
+        }
+
     }
 }
