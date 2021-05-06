2bf1585d0bd4e5b00e61b4d44743295ea353f05a
1826914 <1826914@stud.hs-mannheim.de>
2021-05-06 21:14:29 +0200

    added security header. uncommented excel test

--- CTT-Github/src/test/java/de/hs_mannheim/informatik/ct/end_to_end/ImportRoomsFromFile.java
+++ CTT-Github/src/test/java/de/hs_mannheim/informatik/ct/end_to_end/ImportRoomsFromFile.java
@@ -1,5 +1,23 @@
 package de.hs_mannheim.informatik.ct.end_to_end;
 
+/*
+ * Corona Tracking Tool der Hochschule Mannheim
+ * Copyright (C) 2021 Hochschule Mannheim
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import de.hs_mannheim.informatik.ct.RoomServiceHelper;
 import de.hs_mannheim.informatik.ct.model.Room;
 import de.hs_mannheim.informatik.ct.persistence.services.RoomService;
@@ -171,42 +189,13 @@ public class ImportRoomsFromFile {
             }
         }
     }
-/**
-    // Excel tests
-    @Test
-    public void importExcel() throws IOException{
-        String testExcelPath = "excelForTest.xlsm";
-        InputStream is = inputStreamFromTestExcel(testExcelPath);
-
-        roomService.importFromExcel(is);
-    }
-
-    @Test
-    public void importExcel_EmptyFile() throws IOException {
-        String testExcelPath = "excelForTest-empty.xlsm";
-        InputStream is = inputStreamFromTestExcel(testExcelPath);
-
-        assertThrows(Exception.class, () -> {
-            roomService.importFromExcel(is);
-        });
-    }
-
-    @Test
-    public void importExcel_WrongRoomCapacityType() throws IOException {
-        String testExcelPath = "excelForTest-wrongRoomCapacityType.xlsm";
-        InputStream is = inputStreamFromTestExcel(testExcelPath);
-
-        assertThrows(Exception.class, () -> {
-            roomService.importFromExcel(is);
-        });
-    }
 
     @Test
     public void importExcel_overriteMultipleRooms() throws Exception {
         String testExcelPath = "excelForTest.xlsm";
         // needs to be changed if the values in the testfile change.
         String[] roomNames = new String[]{"A-101", "A-102"};
-        List<String[]> testRoomData = createRoomData(roomNames);
+        List<String[]> testRoomData = helper.createRoomData(roomNames);
         String[] initialTestRoomPins = extractRoomPins(roomNames);
 
         roomService.saveRoom(
@@ -215,7 +204,7 @@ public class ImportRoomsFromFile {
         roomService.saveRoom(
                 new Room("A-102", "A", 8)
         );
-        InputStream is = inputStreamFromTestExcel(testExcelPath);
+        InputStream is = helper.inputStreamFromTestExcel(testExcelPath);
         roomService.importFromExcel(is);
 
         String[] newTestRoomPins = extractRoomPins(roomNames);
