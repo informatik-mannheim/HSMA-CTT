979fb6d7095dcaac34c0f1d9447ead44f70de9b6
1826914 <1826914@stud.hs-mannheim.de>
2021-07-07 22:23:15 +0200

    added test with data from livesytem

--- src/test/java/de/hs_mannheim/informatik/ct/model/RoomVisitTest.java
+++ src/test/java/de/hs_mannheim/informatik/ct/model/RoomVisitTest.java
@@ -70,15 +70,13 @@ public class RoomVisitTest {
 
     @Test
     void checkout(){
-        RoomVisit checkOutSourceProvider = new RoomVisit(visitor, room, TimeUtil.convertToDate(now));
-
         RoomVisit roomVisit = new RoomVisit(
                 room,
                 null,
                 TimeUtil.convertToDate(now.minusDays(1)),
                 TimeUtil.convertToDate(now),
                 visitor,
-                checkOutSourceProvider.getCheckOutSource());
+                CheckOutSource.NotCheckedOut);
 
         roomVisit.checkOut(TimeUtil.convertToDate(now), CheckOutSource.RoomReset);
 
@@ -97,11 +95,37 @@ public class RoomVisitTest {
     }
 
     @Test
-    void checkout_endDate(){
-        Date tomorrow = TimeUtil.convertToDate(now.plus(Duration.ofDays(1)));
-        Date yesterday = TimeUtil.convertToDate(now.minus(Duration.ofDays(1)));
+    void automaticCheckOutFailureData() {
+        /*
+        this Data caused users not getting checked for an unknown cause
+        id  | end_date | start_date                 | room_name | visitor_id | check_out_source
+        ---- - +-------------------------+-------------------------+-----------+------------+------------------
+        188 |          |2021 - 07 - 01 12:29:54.322 | L114      | 187        | 0
+        192 |          |2021 - 07 - 01 12:42:29.346 | L206      | 191        | 0
+        194 |          |2021 - 07 - 01 12:43:08.854 | L206      | 193        | 0
+        133 |          |2021 - 06 - 20 12:07:31.253 | K019a     | 27         |
+        123 |          |2021 - 06 - 17 15:05:49.142 | L312      | 122        |
+        */
+        Date validDate = TimeUtil.convertToDate(now);
 
+        Visitor visitor1 = new Visitor(187L, "v1");
+        Visitor visitor2 = new Visitor(191L, "v2");
+        Visitor visitor3 = new Visitor(193L, "v3");
+        Visitor visitor4 = new Visitor(27L, "v4");
+        Visitor visitor5 = new Visitor(122L, "v5");
 
+        Room room1 = new Room("L114", "L", 10);
+        Room room2 = new Room("L206", "L", 10);
+        Room room4 = new Room("K019a", "K", 10);
+        Room room5 = new Room("L312", "L", 10);
+
+        Visitor[] visitors = new Visitor[]{visitor1, visitor2, visitor3, visitor4, visitor5};
+        Room[] rooms = new Room[]{room1, room2, room2, room4, room5};
+
+        for(int i = 0; i < visitors.length; i++){
+            RoomVisit roomVisit = checkOutCall(validDate, CheckOutSource.AutomaticCheckout, rooms[i], visitors[i]);
+            assertThat(roomVisit.getCheckOutSource(), not(CheckOutSource.NotCheckedOut));
+        }
     }
 
     private RoomVisit checkOutCall(Date checkOutDate, CheckOutSource checkOutSource){
@@ -111,6 +135,20 @@ public class RoomVisitTest {
                 TimeUtil.convertToDate(now.minusDays(1)),
                 checkOutDate,
                 visitor,
+                checkOutSource
+        );
+
+        roomVisit.checkOut(TimeUtil.convertToDate(now), CheckOutSource.RoomReset);
+        return roomVisit;
+    }
+
+    private RoomVisit checkOutCall(Date checkOutDate, CheckOutSource checkOutSource, Room customRoom, Visitor customVisitor){
+        RoomVisit roomVisit = new RoomVisit(
+                customRoom,
+                null,
+                TimeUtil.convertToDate(now.minusDays(1)),
+                checkOutDate,
+                customVisitor,
                 checkOutSource
         );
 
