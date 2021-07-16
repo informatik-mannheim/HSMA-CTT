1390306a8295dc4ea9edb5d1a0c25c7960f5c65f
Gabriel E <51362934+1826914@users.noreply.github.com>
2021-07-16 13:08:11 +0200

    Automated visitor check out fix (#150)
    * created todos
    
    * created first test. needs debugging
    
    * basic tests working
    
    * added RoomVisitRepo tests. Needs refactoring
    
    * strict typing
    
    * added final tests. refactored. commented
    
    * extended enddate tests
    
    * added alternative setup method for repository tests
    
    * fixed room reset with expired visit test
    
    * first checkout test
    
    * created todos
    
    * created first test. needs debugging
    
    * basic tests working
    
    * added RoomVisitRepo tests. Needs refactoring
    
    * strict typing
    
    * added final tests. refactored. commented
    
    * extended enddate tests
    
    * added alternative setup method for repository tests
    
    * fixed room reset with expired visit test
    
    * first checkout test
    
    * debugging
    
    * Feature/room reset test (#139)
    
    * created todos
    
    * created first test. needs debugging
    
    * basic tests working
    
    * added RoomVisitRepo tests. Needs refactoring
    
    * strict typing
    
    * added final tests. refactored. commented
    
    * extended enddate tests
    
    * added alternative setup method for repository tests
    
    * fixed room reset with expired visit test
    
    * fixed checkout Method. Altered tests
    
    * outsourced CheckOutSource enum
    
    * fixed CheckOutSource calls
    
    * added test with data from livesytem
    
    * created todos
    
    * created first test. needs debugging
    
    * basic tests working
    
    * added RoomVisitRepo tests. Needs refactoring
    
    * added final tests. refactored. commented
    
    * extended enddate tests
    
    * added alternative setup method for repository tests
    
    * fixed room reset with expired visit test
    
    * first checkout test
    
    * debugging
    
    * created todos
    
    * created first test. needs debugging
    
    * basic tests working
    
    * added final tests. refactored. commented
    
    * extended enddate tests
    
    * Feature/room reset test (#139)
    
    * created todos
    
    * created first test. needs debugging
    
    * basic tests working
    
    * added RoomVisitRepo tests. Needs refactoring
    
    * strict typing
    
    * added final tests. refactored. commented
    
    * extended enddate tests
    
    * added alternative setup method for repository tests
    
    * fixed room reset with expired visit test
    
    * fixed checkout Method. Altered tests
    
    * outsourced CheckOutSource enum
    
    * fixed CheckOutSource calls
    
    * added test with data from livesytem
    
    * formated code
    
    * added singOutAllVisitors test. refactored code and comments
    
    * added assert on enddate for every test. fixed comment

--- src/test/java/de/hs_mannheim/informatik/ct/persistence/services/RoomVisitServiceTest.java
+++ src/test/java/de/hs_mannheim/informatik/ct/persistence/services/RoomVisitServiceTest.java
@@ -19,6 +19,7 @@ package de.hs_mannheim.informatik.ct.persistence.services;
  */
 
 
+import de.hs_mannheim.informatik.ct.model.CheckOutSource;
 import de.hs_mannheim.informatik.ct.model.Room;
 import de.hs_mannheim.informatik.ct.model.RoomVisit;
 import de.hs_mannheim.informatik.ct.model.Visitor;
@@ -309,7 +310,7 @@ class RoomVisitServiceTest {
 
         assertThat(roomVisitService.getVisitorCount(testRoom), equalTo(0));
         for(RoomVisit visit : visits){
-            assertThat(visit.getCheckOutSource(), not(RoomVisit.CheckOutSource.NotCheckedOut));
+            assertThat(visit.getCheckOutSource(), not(CheckOutSource.NotCheckedOut));
         }
     }
 
@@ -334,7 +335,7 @@ class RoomVisitServiceTest {
 
         roomVisitService.resetRoom(testRoom);
 
-        assertThat(visit.getCheckOutSource(), equalTo(RoomVisit.CheckOutSource.RoomReset));
+        assertThat(visit.getCheckOutSource(), equalTo(CheckOutSource.RoomReset));
     }
 
     private void testAutoCheckout(@NonNull LocalDateTime checkInTime, @NonNull LocalTime forcedEndTime, LocalDateTime expectedCheckoutTime) {
@@ -364,7 +365,7 @@ class RoomVisitServiceTest {
             assertThat(capturedArgument.size(), equalTo(1));
             val checkedOutVisit = capturedArgument.get(0);
             assertThat(convertToLocalDateTime(checkedOutVisit.getEndDate()), equalTo(expectedCheckoutTime));
-            assertThat(checkedOutVisit.getCheckOutSource(), equalTo(RoomVisit.CheckOutSource.AutomaticCheckout));
+            assertThat(checkedOutVisit.getCheckOutSource(), equalTo(CheckOutSource.AutomaticCheckout));
         }
     }
 }
