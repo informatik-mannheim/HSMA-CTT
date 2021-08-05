4e8d71a217143a19c2d6e034da5069d4d00bda7b
Linus <linus.bennin@stud.hs-mannheim.de>
2021-08-05 14:23:02 +0200

    Rebase check
    added: RoomFull-, InvalidExternalUser-, InvalidRoomPin-, UnsupportedEncodingException
    
    Some tests were changed

--- src/main/java/de/hs_mannheim/informatik/ct/controller/ErrorController.java
+++ src/main/java/de/hs_mannheim/informatik/ct/controller/ErrorController.java
@@ -1,50 +1,125 @@
 package de.hs_mannheim.informatik.ct.controller;
 
+/*
+ * Corona Tracking Tool der Hochschule Mannheim
+ * Copyright (C) 2021 Hochschule Mannheim
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import de.hs_mannheim.informatik.ct.controller.exception.InvalidRoomPinException;
 import de.hs_mannheim.informatik.ct.persistence.EventNotFoundException;
 import de.hs_mannheim.informatik.ct.persistence.InvalidEmailException;
+import de.hs_mannheim.informatik.ct.persistence.InvalidExternalUserdataException;
+import de.hs_mannheim.informatik.ct.persistence.RoomFullException;
+import org.apache.xpath.operations.Mod;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.text.AttributedString;
 
 @ControllerAdvice
 public class ErrorController {
     @ExceptionHandler({RoomController.RoomNotFoundException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleRoomNotFoundException(RoomController.RoomNotFoundException roomNotFound, WebRequest request) {
-        return "error/roomNotFound";
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+//    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Room not found")
+    public String handleRoomNotFoundException(Model model) {
+        model.addAttribute("errorMessage", "Room not found");
+        return "error/errorTemplate";
     }
 
     @ExceptionHandler({RoomController.InvalidFileUploadException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleInvalidFileUploadException(RoomController.InvalidFileUploadException roomNotFound, WebRequest request) {
-        return "error/invalidFileUpload";
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+//    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid file upload")
+    public String handleInvalidFileUploadException(Model model) {
+        model.addAttribute("errorMessage", "File upload is invalid");
+        return "error/errorTemplate";
     }
 
     @ExceptionHandler({RoomController.VisitorNotFoundException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleVisitorNotFoundException(RoomController.VisitorNotFoundException roomNotFound, WebRequest request) {
-        return "error/visitorNotFound";
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+//    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "visitor not found")
+    public String handleVisitorNotFoundException(Model model) {
+        model.addAttribute("errorMessage", "Visitor not found");
+
+        return "error/errorTemplate";
     }
 
     @ExceptionHandler({InvalidEmailException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleInvalidEmail(InvalidEmailException invalidEmail, WebRequest request) {
-        return "error/invalidEmail";
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+//    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "invalid email")
+    public String handleInvalidEmail(Model model) {
+        model.addAttribute("errorMessage", "Email is invalid");
+
+        return "error/errorTemplate";
     }
 
     @ExceptionHandler({EventNotFoundException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleEventNotFound(EventNotFoundException eventNotFound, WebRequest request) {
-        return "error/eventNotFound";
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+//    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "event not found")
+    public String handleEventNotFound(Model model) {
+        model.addAttribute("errorMessage", "Event not found");
+
+        return "error/errorTemplate";
+    }
+
+    @ExceptionHandler({UnsupportedEncodingException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+//    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "unsupported encoding")
+    public String handleUnsupportedEncodingException(Model model) {
+        model.addAttribute("errorMessage", "Encoding is not supported");
+
+        return "error/errorTemplate";
+    }
+
+    @ExceptionHandler({RoomFullException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+//    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "room full")
+    public String handleRoomFullException(Model model) {
+        model.addAttribute("errorMessage", "The room is full");
+
+        return "error/errorTemplate";
+    }
+
+    @ExceptionHandler({InvalidExternalUserdataException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+//    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "invalid external userdata")
+    public String handleInvalidExternalUserdataException(Model model) {
+        model.addAttribute("errorMessage", "Userdata is invalid");
+
+        return "error/errorTemplate";
+    }
+
+    @ExceptionHandler({InvalidRoomPinException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+//    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "invalid room pin")
+    public String handleInvalidRoomPinException(Model model) {
+        model.addAttribute("errorMessage", "Room pin is invalid");
+
+        return "error/errorTemplate";
     }
 
     @RequestMapping("/error")
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+//    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         System.out.println("Request Status: " + status);
@@ -60,8 +135,11 @@ public class ErrorController {
     }
 
     @ExceptionHandler({Exception.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+//    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "unknown error")
     public String anyException() {
-        return "index";
+        return "error";
     }
 
 }
+
