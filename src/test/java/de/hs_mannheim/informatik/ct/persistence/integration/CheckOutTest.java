970f19e4649ea1b103f7e98e0d75971107df38de
1826914 <1826914@stud.hs-mannheim.de>
2021-09-14 16:40:38 +0200

    changed test configuration to mock datetimeservice

--- src/test/java/de/hs_mannheim/informatik/ct/persistence/integration/CheckOutTest.java
+++ src/test/java/de/hs_mannheim/informatik/ct/persistence/integration/CheckOutTest.java
@@ -38,10 +38,12 @@ import de.hs_mannheim.informatik.ct.util.TimeUtil;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -49,6 +51,8 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class CheckOutTest {
@@ -60,23 +64,24 @@ public class CheckOutTest {
         }
 
         @Bean
-        public DateTimeService dateTimeService() {
-            return new DateTimeService();
-        }
-
-        @Bean
         public EventVisitService eventVisitService() {
             return new EventVisitService();
         }
 
         @Bean
-        public ScheduledMaintenanceTasks scheduledMaintenanceTasks() {
+        public ScheduledMaintenanceTasks scheduledMaintenanceTasks(){
             return new ScheduledMaintenanceTasks();
         }
     }
 
-    @Autowired
+    @MockBean
+    public DateTimeService dateTimeService;
+
+    @InjectMocks
     private RoomVisitService roomVisitService;
+
+    @Autowired
+    private ScheduledMaintenanceTasks scheduledMaintenanceTasks;
 
     @Autowired
     private TestEntityManager entityManager;
@@ -87,80 +92,7 @@ public class CheckOutTest {
     @Autowired
     private VisitorRepository visitorRepository;
 
-    @Autowired
-    private ScheduledMaintenanceTasks scheduledMaintenanceTasks;
-
     private LocalDateTime now = LocalDateTime.now();
-
-    @Test
-    void scheduledMaintenanceTasksTest() {
-        Room room = entityManager.persist(new Room("A001", "A", 10));
-        List<RoomVisit> visitsToday = Arrays.asList(
-                entityManager.persist(new RoomVisit(
-                        entityManager.persist(new Visitor("1")),
-                        room,
-                        //TimeUtil.convertToDate(LocalDateTime.parse("03:54:59"))
-                        TimeUtil.convertToDate(now.withHour(3).withMinute(54).withSecond(59))
-                )),
-                entityManager.persist(new RoomVisit(
-                        entityManager.persist(new Visitor("2")),
-                        room,
-                        //TimeUtil.convertToDate(LocalDateTime.parse("00:00:00"))
-                        TimeUtil.convertToDate(now.withHour(0).withMinute(0).withSecond(0))
-                )),
-                entityManager.persist(new RoomVisit(
-                        entityManager.persist(new Visitor("3")),
-                        room,
-                        //TimeUtil.convertToDate(LocalDateTime.parse("12:00:00"))
-                        TimeUtil.convertToDate(now.withHour(12).withMinute(0).withSecond(0))
-                ))
-        );
-        List<RoomVisit> visitsYesterday = Arrays.asList(
-                entityManager.persist(new RoomVisit(
-                        entityManager.persist(new Visitor("4")),
-                        room,
-                        //TimeUtil.convertToDate(LocalDateTime.parse("23:59:59").minusDays(1))
-                        TimeUtil.convertToDate(now.withHour(23).withMinute(59).withSecond(59).minusDays(1))
-                )),
-                entityManager.persist(new RoomVisit(
-                        entityManager.persist(new Visitor("5")),
-                        room,
-                        //TimeUtil.convertToDate(LocalDateTime.parse("00:00:01").minusDays(1))
-                        TimeUtil.convertToDate(now.withHour(0).withMinute(0).withSecond(1).minusDays(1))
-                ))
-        );
-
-        roomVisitRepository.saveAll(visitsToday);
-        roomVisitRepository.saveAll(visitsYesterday);
-
-        // this ensures that RoomVisitService::checkOutAllVisitors works as if it was 3:55. This is the time the cron job should run
-        //when(dateTimeService.getNow()).thenReturn(LocalDateTime.parse("03:55:00"));
-
-        // run checkout routine
-        scheduledMaintenanceTasks.doMaintenance();
-
-        assertThat(roomVisitRepository.findNotCheckedOutVisits(room),
-                everyItem(in(visitsToday)));
-        assertThat(roomVisitRepository.findNotCheckedOutVisits(room),
-                not(everyItem(in(visitsYesterday))));
-
-        visitsToday.stream().allMatch(
-                visit -> {
-                    return
-                            visit.getCheckOutSource().equals(CheckOutSource.NotCheckedOut) &&
-                                    visit.getEndDate() == null;
-                }
-        );
-
-        visitsYesterday.stream().allMatch(
-                visit -> {
-                    return
-                            visit.getCheckOutSource().equals(CheckOutSource.AutomaticCheckout) &&
-                                    visit.getEndDate() != null;
-                }
-        );
-
-    }
 
     @Test
     void checkout() {
@@ -195,8 +127,11 @@ public class CheckOutTest {
         );
 
         roomVisitRepository.save(roomVisit);
-        // forcing check out at 00:00
-        scheduledMaintenanceTasks.signOutAllVisitors(LocalTime.of(0, 0));
+
+        // this ensures that RoomVisitService::checkOutAllVisitors works as if it was 3:55. This is the time the cron job should run
+        when(dateTimeService.getNow()).thenReturn(LocalDateTime.now().withHour(3).withMinute(55));
+
+        scheduledMaintenanceTasks.doMaintenance();
 
         assertThat(roomVisitRepository.getRoomVisitorCount(room), is(0));
         assertThat(roomVisit.getCheckOutSource(), is(CheckOutSource.AutomaticCheckout));
@@ -225,5 +160,67 @@ public class CheckOutTest {
         assertThat(roomVisitRepository.getRoomVisitorCount(room), is(0));
         assertThat(roomVisit.getCheckOutSource(), is(CheckOutSource.AutomaticCheckout));
         assertThat(roomVisit.getEndDate(), not(nullValue()));
+    }
+
+    @Test
+    void scheduledMaintenanceTasksTest() {
+        Room room = entityManager.persist(new Room("A001", "A", 10));
+        // visitors that checked in today and should not get
+        List<RoomVisit> visitsToday = Arrays.asList(
+                entityManager.persist(new RoomVisit(
+                        entityManager.persist(new Visitor("1")),
+                        room,
+                        TimeUtil.convertToDate(now.withHour(3).withMinute(54).withSecond(59))
+                )),
+                entityManager.persist(new RoomVisit(
+                        entityManager.persist(new Visitor("2")),
+                        room,
+                        TimeUtil.convertToDate(now.withHour(0).withMinute(0).withSecond(0))
+                )),
+                entityManager.persist(new RoomVisit(
+                        entityManager.persist(new Visitor("3")),
+                        room,
+                        TimeUtil.convertToDate(now.withHour(12).withMinute(0).withSecond(0))
+                ))
+        );
+        List<RoomVisit> visitsYesterday = Arrays.asList(
+                entityManager.persist(new RoomVisit(
+                        entityManager.persist(new Visitor("4")),
+                        room,
+                        TimeUtil.convertToDate(now.withHour(23).withMinute(59).withSecond(59).minusDays(1))
+                )),
+                entityManager.persist(new RoomVisit(
+                        entityManager.persist(new Visitor("5")),
+                        room,
+                        TimeUtil.convertToDate(now.withHour(0).withMinute(0).withSecond(1).minusDays(1))
+                ))
+        );
+
+        roomVisitRepository.saveAll(visitsToday);
+        roomVisitRepository.saveAll(visitsYesterday);
+
+        // this ensures that RoomVisitService::checkOutAllVisitors works as if it was 3:55. This is the time the cron job should run
+        when(dateTimeService.getNow()).thenReturn(LocalDateTime.now().withHour(3).withMinute(55));
+
+        // run check out task
+        scheduledMaintenanceTasks.doMaintenance();
+
+
+        assertThat(roomVisitRepository.findNotCheckedOutVisits(room),
+                everyItem(in(visitsToday)));
+        assertThat(roomVisitRepository.findNotCheckedOutVisits(room),
+                not(everyItem(in(visitsYesterday))));
+
+        visitsToday.stream().allMatch(
+                visit ->
+                        visit.getCheckOutSource().equals(CheckOutSource.NotCheckedOut) &&
+                                visit.getEndDate() == null
+        );
+
+        visitsYesterday.stream().allMatch(
+                visit ->
+                        visit.getCheckOutSource().equals(CheckOutSource.AutomaticCheckout) &&
+                                visit.getEndDate() != null
+        );
     }
 }
