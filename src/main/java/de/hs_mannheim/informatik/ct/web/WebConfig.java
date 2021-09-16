1b69959ea3edbacde93500b357d13fbdcc225455
m.stock <36995969+maxones25@users.noreply.github.com>
2021-09-16 14:10:24 +0200

    wrap interceptor instance in Bean method

--- src/main/java/de/hs_mannheim/informatik/ct/web/WebConfig.java
+++ src/main/java/de/hs_mannheim/informatik/ct/web/WebConfig.java
@@ -1,6 +1,7 @@
 package de.hs_mannheim.informatik.ct.web;
 
 import de.hs_mannheim.informatik.ct.controller.interceptor.CheckInInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,8 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new CheckInInterceptor())
+        registry.addInterceptor(checkInInterceptor())
                 .addPathPatterns("/")
                 .addPathPatterns("/r/**");
+    }
+
+    @Bean
+    public CheckInInterceptor checkInInterceptor(){
+        return new CheckInInterceptor();
     }
 }
