956caaa3d18e9b46a62524ccfa1da435ff42de56
m.stock <36995969+maxones25@users.noreply.github.com>
2022-01-27 16:47:10 +0100

    Use Regex Pattern

--- src/main/java/de/hs_mannheim/informatik/ct/util/MailAddress.java
+++ src/main/java/de/hs_mannheim/informatik/ct/util/MailAddress.java
@@ -1,5 +1,6 @@
 package de.hs_mannheim.informatik.ct.util;
 
+import de.hs_mannheim.informatik.ct.persistence.InvalidEmailException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.val;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @AllArgsConstructor @Getter
 public class MailAddress {
@@ -17,14 +19,18 @@ public class MailAddress {
     private String domain;
     private String tld;
 
-    public static MailAddress parse(String mailAddress) throws MailAddressInvalidException {
-        var chunks = split(mailAddress, "@");
-        if(chunks.size()!=2) throw new MailAddressInvalidException();
-        val name = chunks.get(0);
-        chunks = split(chunks.get(1), "\\.");
-        if(chunks.size()<2) throw new MailAddressInvalidException();
-        val tld = chunks.get(chunks.size() - 1);
-        val domain = joinWithoutLast(chunks, ".");
+    private static final Pattern email_splitter_regex = Pattern.compile("(.*)@(.*)\\.(.*)$");
+
+    public static MailAddress parse(String mailAddress) throws InvalidEmailException {
+        val match = email_splitter_regex.matcher(mailAddress);
+        if(!match.matches()) {
+            throw new InvalidEmailException();
+        }
+
+        val name = match.group(1);
+        val domain = match.group(2);
+        val tld = match.group(3);
+
         return new MailAddress(mailAddress, name, domain, tld);
     }
 
