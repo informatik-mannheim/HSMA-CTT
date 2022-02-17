efeae3bf8861ade35c748d9af1cb1920979ff4df
m.stock <36995969+maxones25@users.noreply.github.com>
2022-02-17 13:29:04 +0100

    Return invalid email and log error instead of throw Exception

--- src/main/java/de/hs_mannheim/informatik/ct/util/MailAddress.java
+++ src/main/java/de/hs_mannheim/informatik/ct/util/MailAddress.java
@@ -3,6 +3,7 @@ package de.hs_mannheim.informatik.ct.util;
 import de.hs_mannheim.informatik.ct.persistence.InvalidEmailException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import lombok.var;
 import org.springframework.http.HttpStatus;
@@ -10,28 +11,35 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @AllArgsConstructor @Getter
+@Slf4j
 public class MailAddress {
     private String mailAddress;
     private String name;
     private String domain;
     private String tld;
 
-    private static final Pattern email_splitter_regex = Pattern.compile("(.*)@(.*)\\.(.*)$");
+    private static final String EMAIL_REGEX = "(.*)@(.*)\\.(.*)$";
+    private static final String DEFAULT_INVALID_MAIL = "invalid-email@invalid.org";
 
-    public static MailAddress parse(String mailAddress) throws InvalidEmailException {
-        val match = email_splitter_regex.matcher(mailAddress);
-        if(!match.matches()) {
-            throw new InvalidEmailException();
-        }
-
+    public static MailAddress parse(String mailAddress) {
+        val match = splitMailAddress(mailAddress);
         val name = match.group(1);
         val domain = match.group(2);
         val tld = match.group(3);
-
         return new MailAddress(mailAddress, name, domain, tld);
+    }
+
+    private static Matcher splitMailAddress(String mailAddress){
+        Matcher match = Pattern.compile(EMAIL_REGEX).matcher(mailAddress);
+        if(!match.matches()) {
+            match = Pattern.compile(EMAIL_REGEX).matcher(DEFAULT_INVALID_MAIL);
+            log.error("support mail address '"+mailAddress+"' is invalid");
+        }
+        return match;
     }
 
     private static List<String> split(String data, String delimiter){
@@ -44,9 +52,5 @@ public class MailAddress {
             domain += glue + data.get(i);
         }
         return domain;
-    }
-
-    @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Mail Address invalid")
-    public static class MailAddressInvalidException extends RuntimeException {
     }
 }
